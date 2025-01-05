f2941e5ee282bc800960c278c85e71f648345ae5
Denny_07 <kriz.dendropark@hotmail.com>
2025-01-05 17:26:28 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SapeArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer;
+
+            Console.WriteLine("What shape wold you like to find the area of? Please enter 'r' rectangle or other key for circle.");
+
+            answer = Console.ReadLine();
+
+            if (answer == "r")
+            {
+                Console.WriteLine("Please enter the height of the rectangle.");
+                float height = float.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
